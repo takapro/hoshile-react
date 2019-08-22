3bb69525f82a9e029549dd6eb330b8d4aba10f71
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-08-22 12:17:27 -0700

    Eject and make it standalone

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = (env, argv) => {
+  const is_dev = argv.mode !== 'production';
+  return {
+    mode: is_dev ? 'development' : 'production',
+    devtool: is_dev ? 'inline-source-map' : 'none',
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, './public'),
+      publicPath: '/'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, use: [{ loader: 'ts-loader' }] }
+      ]
+    },
+    devServer: {
+      historyApiFallback: true
+    }
+  };
+};
